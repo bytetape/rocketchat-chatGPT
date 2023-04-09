1ed8cd5eca8dae5f9baef518bd1f6760b0919ea1
bytetape <130202349+bytetape@users.noreply.github.com>
2023-04-09 16:42:10 +0800

    Update installDockerComposeAndRocketchat_debian.sh

--- installDockerComposeAndRocketchat_debian.sh
+++ installDockerComposeAndRocketchat_debian.sh
@@ -1,5 +1,6 @@
 sudo -i
 apt-get update && \
+apt install curl sudo && \
 apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release && \
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg && \
 echo \
