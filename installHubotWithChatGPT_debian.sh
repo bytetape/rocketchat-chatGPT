bbe72c724bc54afc9f1be272757056fcd98627ea
bytetape <130202349+bytetape@users.noreply.github.com>
2023-04-09 17:49:56 +0800

    Update installHubotWithChatGPT_debian.sh

--- installHubotWithChatGPT_debian.sh
+++ installHubotWithChatGPT_debian.sh
@@ -37,16 +37,11 @@
 sudo apt-get update
 sudo apt update && sudo apt upgrade -y
 sudo apt install curl sudo git -y
-sudo rm -rf hubot-rocketchat-boilerplate
-sudo git clone https://github.com/RocketChat/hubot-rocketchat-boilerplate
-cd hubot-rocketchat-boilerplate
+sudo rm -rf rocketchat_chatGPT
+sudo git clone https://github.com/bytetape/rocketchat_chatGPT
+cd rocketchat_chatGPT
 sudo npm install
-sudo npm install mkdirp@latest
-sudo npm install connect@latest
-sudo npm install coffeescript@latest
 sudo npm install node-fetch --save
-sudo curl -sSL -o .env https://raw.githubusercontent.com/bytetape/rocketchat_chatGPT/main/.env
-sudo curl -sSL -o scripts/script_chatGPT.js https://raw.githubusercontent.com/bytetape/rocketchat_chatGPT/main/script_chatGPT.js
 read -p "请输入 openAI API:" API
 sudo sed -i "s/YOUR API KEY/$API/g" scripts/script_chatGPT.js
 sudo source .env
