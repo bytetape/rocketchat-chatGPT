749e5a762e77268cb80f64d305c6fed8bbe2628c
bytetape <130202349+bytetape@users.noreply.github.com>
2023-04-09 16:24:20 +0800

    Update installDockerComposeAndRocketchat_debian.sh

--- installDockerComposeAndRocketchat_debian.sh
+++ installDockerComposeAndRocketchat_debian.sh
@@ -5,6 +5,7 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o 
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null  && \
+apt-get update && \
 apt-get install -y docker-ce docker-ce-cli containerd.io && \
 systemctl start docker && \
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
